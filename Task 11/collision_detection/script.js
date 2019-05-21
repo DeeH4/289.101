e245b8379737956f67f6e9b3251f3f39d4bedffa
DeeH4 <48193057+DeeH4@users.noreply.github.com>
2019-05-21 18:04:22 +1200

    started task 11

--- Task 11/collision_detection/script.js
+++ Task 11/collision_detection/script.js
@@ -9,3 +9,62 @@ Next, add the following features:
 - the puck 'teleports' to the opposite boundary when it crosses one (think asteroids/snake)
 
 */
+
+var puck = document.getElementById('puck')
+var puck_x = 210;
+var puck_y = 210;
+var speed = 50;
+
+
+puck.style.left = puck_x + 'px';
+puck.style.top = puck_y + 'px';
+
+function coordinateLogs() {
+    console.log('x=' + puck_x + 'y=' + puck_y);
+}
+
+function collisionCheck(){
+    // has the puck passed the left edge of the hole
+    if ( puck_x > 250 && puck_x < 350 && puck_y > 150 && puck_y < 250 ) {
+        console.log('AHHHHHH');
+        }
+}
+
+function puckTeleport() {
+    // if puck goes outside the arena it teleports
+    if ( puck_x > 600 && puck_x < 400 )
+        console.log('start again');
+}
+
+document.getElementById('left').addEventListener('click', function(){
+    puck_x -= speed;
+    document.getElementById('puck').style.left = puck_x + 'px';
+    coordinateLogs();
+    collisionCheck();
+    puckTeleport();
+    
+});
+
+document.getElementById('right').addEventListener('click', function(){
+    puck_x += speed;
+    document.getElementById('puck').style.left = puck_x + 'px';
+    coordinateLogs();
+    collisionCheck();
+    puckTeleport()
+});
+
+document.getElementById('up').addEventListener('click', function(){
+    puck_y -= speed;
+    document.getElementById('puck').style.top = puck_y + 'px';
+    coordinateLogs();
+    collisionCheck();
+    puckTeleport()
+});
+
+document.getElementById('down').addEventListener('click', function(){
+    puck_y += speed;
+    document.getElementById('puck').style.top = puck_y + 'px';
+    coordinateLogs();
+    collisionCheck();
+    puckTeleport()
+});
